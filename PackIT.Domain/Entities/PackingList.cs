1a96b28716f8215fac6ca01afd71a61c6ceef32e
Krzysztof Bulanowski <krzysztof.bulanowski@outlook.com>
2023-01-15 22:01:52 +0100

    Add aggregate and domain event

--- PackIT.Domain/Entities/PackingList.cs
+++ PackIT.Domain/Entities/PackingList.cs
@@ -1,14 +1,16 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using PackIT.Domain.Events;
 using PackIT.Domain.Exceptions;
 using PackIT.Domain.ValueObjects;
+using PackIT.Shared.Abstractions.Domain;
 
 namespace PackIT.Domain.Entities;
 
-public class PackingList
+public class PackingList : AggregateRoot<PackingListId>
 {
-    public Guid Id { get; private set; }
+    public PackingListId Id { get; private set; }
 
     private PackingListName _name;
     private Localization _localization;
@@ -31,5 +33,6 @@ public class PackingList
             throw new PackingItemAlreadyExistsException(_name, item.Name);
 
         _items.AddLast(item);
+        AddEvent(new PackingItemAdded(this, item));
     }
 }
